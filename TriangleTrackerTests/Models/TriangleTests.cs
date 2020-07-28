6834def15be4767783f1826be8cea448b10f680f
David Boedigheimer <stealth259@gmail.com>
2020-07-28 15:48:07 -0700

    Add start for main page, with Brian's help.

--- TriangleTrackerTests/Models/TriangleTests.cs
+++ TriangleTrackerTests/Models/TriangleTests.cs
@@ -1,15 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TriangleTracker;
 
-namespace TriangleTrackerTests {
+namespace TriangleTrackerTests
+{
     [TestClass]
-    public class TriangleTests {
+    public class TriangleTests
+    {
         // Test methods go here
         [TestMethod]
-        public void IsEquilateral_AllSidesEqual_True () 
+        public void IsEquilateral_AllSidesEqual_True()
         {
             // any necessary logic to prep for test; instantiating new classes, etc.
-            Assert.AreEqual ("EXPECTED RESULT, CODE TO TEST");
+            Assert.AreEqual();
         }
     }
 }
